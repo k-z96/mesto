c92175a5d06f0d474f0dad6d8f09b8209c5cd7bf
Kate Zibrova <zibrovakate@yandex.ru>
2023-04-21 14:42:03 +0400

    feature: add card and card zoom popups

--- scripts/index.js
+++ scripts/index.js
@@ -1,51 +1,189 @@
-//Declare
-let popupElement = document.querySelector('.popup');
+//addInfo popup
+const addInfoPopup = document.querySelector('.popup_form-edit');
+const popupAddInfoButton = document.querySelector('.profile__edit-button');
 
-let popupAddButton = document.querySelector('.profile__edit-button');
+const popupCloseInfoButton = addInfoPopup.querySelector('.popup__form-close');
 
-let popupCloseButton = document.querySelector('.popup__close-icon');
+const addInfoForm = addInfoPopup.querySelector('.popup__add-info-form');
 
-
-let formElement = document.querySelector('.popup__form');
-
-let nameInput = document.querySelector('#name');  
-let jobInput = document.querySelector('#occupation'); 
+let nameInput = addInfoForm.querySelector('#name');
+let jobInput = addInfoForm.querySelector('#occupation');
 
 let profileName = document.querySelector('.profile__name');
 let profileOccupation = document.querySelector('.profile__occupation');
 
+//Open Popup
+function popupOpen(popup) {
+    popup.classList.add('popup_opened');
+}
 
-//Open popup
-function openPopup(popupElement){
-    popupElement.classList.add('popup_opened');
+//Close Popup
+function popupClose(popup) {
+    popup.classList.remove('popup_opened');
+}
+
+//addInfo popup
+function openAddInfoPopup(addInfoPopup) {
     nameInput.value = profileName.textContent;
     jobInput.value = profileOccupation.textContent;
+    popupOpen(addInfoPopup);
 }
 
-//Close popup
-function closePopup(popupElement){
-    popupElement.classList.remove('popup_opened');
-}
+popupAddInfoButton.addEventListener('click', function () {
+    openAddInfoPopup(addInfoPopup);
+});
 
-//Submit
-function handleFormSubmit (evt) {
-    evt.preventDefault(); 
+popupCloseInfoButton.addEventListener('click', function () {
+    popupClose(addInfoPopup);
+});
+
+function handleFormSubmit(evt) {
+    evt.preventDefault();
 
     profileName.textContent = nameInput.value;
-    profileOccupation.textContent = jobInput.value; 
-    
-    closePopup(popupElement);
+    profileOccupation.textContent = jobInput.value;
+
+    popupClose(addInfoPopup);
 }
 
+addInfoPopup.addEventListener('submit', handleFormSubmit);
 
 
-popupAddButton.addEventListener('click', function(){
-    openPopup(popupElement);
+//addCard popup
+const addCardPopup = document.querySelector('.popup_card-add');
+const addCardButton = document.querySelector('.profile__add-button');
+const addCardCloseButton = addCardPopup.querySelector('.popup__card-close');
+
+const addCardForm = addCardPopup.querySelector('.popup__add-card-form');
+const cardNameInput = addCardPopup.querySelector('#cardName');
+const cardLinkInput = addCardPopup.querySelector('#cardLink');
+
+function openAddCardPopup(addCardPopup) {
+    popupOpen(addCardPopup);
+}
+
+addCardButton.addEventListener('click', function () {
+    popupOpen(addCardPopup);
 });
 
-popupCloseButton.addEventListener('click', function(){
-    closePopup(popupElement);
+addCardCloseButton.addEventListener('click', function () {
+    popupClose(addCardPopup);
 });
 
-formElement.addEventListener('submit', handleFormSubmit);
+function handleAddCardSubmit(evt) {
+    evt.preventDefault();
+
+    const name = cardNameInput.value;
+    const link = cardLinkInput.value;
+
+    const cardData = {
+        name,
+        link
+    }
+
+    renderCardElement(createCardElement(cardData));
+
+    popupClose(addCardPopup);
+}
+
+addCardPopup.addEventListener('submit', handleAddCardSubmit);
+
+
+//popup cards
+const initialCards = [
+    {
+        name: 'Озеро Баскунчак',
+        link: 'https://images.unsplash.com/photo-1606589547223-7fd442990d73?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1335&q=80'
+    },
+    {
+        name: 'Карелия',
+        link: 'https://images.unsplash.com/photo-1573156667488-5c0cec674762?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1335&q=80'
+    },
+    {
+        name: 'Даргавс',
+        link: 'https://images.unsplash.com/photo-1597581729358-7429e1b731bb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1335&q=80'
+    },
+    {
+        name: 'Териберка',
+        link: 'https://images.unsplash.com/photo-1610554120720-6439d218c436?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1335&q=80'
+    },
+    {
+        name: 'Светлогорск',
+        link: 'https://images.unsplash.com/photo-1630735980996-bee02c321d19?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80'
+    },
+    {
+        name: 'Камчатский край',
+        link: 'https://images.unsplash.com/photo-1654192541419-eea56035d65d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE3fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=900&q=60'
+    }
+];
+
+const cardTemplate = document.querySelector('#card-template');
+const cardGallery = document.querySelector('.gallery');
+
+
+
+//Create Cards
+function createCardElement(cardData) {
+    const cardElement = cardTemplate.content
+        .querySelector('.card')
+        .cloneNode(true);
+
+    const cardTitle = cardElement.querySelector('.card__title');
+    const cardImage = cardElement.querySelector('.card__image');
+
+    cardTitle.textContent = cardData.name;
+    cardImage.src = cardData.link;
+    cardImage.alt = cardData.name;
+
+    const cardLikeButton = cardElement.querySelector('.card__like-button');
+    const cardDeleteButton = cardElement.querySelector('.card__delete-button');
+
+    const handleLike = () => {
+        cardLikeButton.classList.toggle('card__like_active');
+    }
+
+    const handleDelete = () => {
+        cardElement.remove();
+    }
+
+    cardLikeButton.addEventListener('click', handleLike);
+
+    cardDeleteButton.addEventListener('click', handleDelete);
+
+
+    const zoomPopup = document.querySelector('.popup_zoom');
+    const zoomClose = document.querySelector('.popup__zoom-close');
+
+    const closeZoom = () => {
+        zoomPopup.classList.remove('popup_opened');
+    }
+
+    const openZoom = () => {
+        zoomPopup.classList.toggle('popup_opened');
+
+        let zoomImage = document.querySelector('.popup__zoom-image');
+        let zoomCaption = document.querySelector('.popup__zoom-caption');
+
+        zoomImage.src = cardData.link;
+        zoomImage.alt = cardData.name;
+        zoomCaption.textContent = cardData.name;
+    }
+
+    cardImage.addEventListener('click', openZoom);
+    zoomClose.addEventListener('click', closeZoom);
+
+
+    return cardElement;
+}
+
+function renderCardElement(cardElement) {
+    cardGallery.prepend(cardElement);
+}
+
+initialCards.forEach((card) => {
+    const element = createCardElement(card);
+    renderCardElement(element);
+});
+
+
 
